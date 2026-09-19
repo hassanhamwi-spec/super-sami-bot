bf6451ff36126163891bf4f9c45c8b6a3e34d5d3
hassanhamwi-spec <hassanhamwi@gmail.com>
2026-09-19 11:05:37 +0300

    Keep server running

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Super Sami & Hermes Bot status: Active and Running!");
+const http = require('http');
+const PORT = process.env.PORT || 10000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end('Super Sami & Hermes Bot status: Active and Running!');
+}).listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
